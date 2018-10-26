6e2b964298116d4d026aef7da192fe9d24e2f99f
Berailitz <admin@ohhere.xyz>
2018-10-26 19:36:33 +0800

    Add argv;

--- main.cpp
+++ main.cpp
@@ -1,9 +1,16 @@
 #include "lexer.cpp"
 
-int main()
+int main(int argc, char const *argv[])
 {
     Lexer lexer;
-    lexer.prase("test.cpp");
+    if (argc == 2)
+    {
+        lexer.prase(argv[1]);
+    }
+    else
+    {
+        lexer.prase("test.cpp");
+    }
     cout << endl;
     lexer.print_stat();
     cout << "Press Enter to exit.";
