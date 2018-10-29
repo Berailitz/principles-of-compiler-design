2a929500d92844bfada2ec4c6e8235e5298415ff
Berailitz <admin@ohhere.xyz>
2018-10-29 22:04:28 +0800

    Improve samples;

--- illegal.cpp
+++ illegal.cpp
@@ -7,8 +7,8 @@
 
 int main(int argc, char const *argv[])
 {
-    const int i = ~0., b = 11;
-    double d = 0.3e2 + i;
+    const int _a = ~0., B_ = 11;
+    double d2 = 0.3e2 + _a;
     int j = 0xz1, k = 011;
     char c = '\';
     std::string s = "string value\n";
